8b2ecfe13291dfe970b966be014b4dd7ce62ddb3
Adnan.iftikhar <adnan.iftikhar@txend.com>
2023-09-20 07:15:29 -0700

    Initial commit

---
+++ src/app/environment.prod.ts
@@ -0,0 +1,11 @@
+export  const environment = {
+    production:true,
+    firebase:{
+    apiKey: "AIzaSyDQoi7N8wa2WcMTuPK-pQw2OWyV_ZTCmCQ",
+    authDomain: "clips-7ffcd.firebaseapp.com",
+    projectId: "clips-7ffcd",
+    storageBucket: "clips-7ffcd.appspot.com",
+    messagingSenderId: "392488342943",
+    appId: "1:392488342943:web:8aae2f3f7b5eb8ad9899aa"
+}  
+}
